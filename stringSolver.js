5c8059f10192cbcf35826f6bb6cdd4b8315d1041
Firefall-Aric <dropshipmk2@gmail.com>
2019-10-23 13:41:46 -0400

    Refactoring + working implementation

--- stringSolver.js
+++ stringSolver.js
@@ -1,135 +1,86 @@
 /* eslint complexity: off*/
-function multiply(multiplier, multiplicand) {
-  return multiplier * multiplicand;
-}
-
-function divide(dividend, divisor) {
-  return dividend / divisor;
-}
-
-function addition(addend_1, addend_2) {
-  return addend_1 + addend_2;
-}
-
-function subtraction(minued, subtrahend) {
-  return minued - subtrahend;
-}
-
 // PEMDAS
 function solve(equation) {
   // Break down parenthesis
-  var matches = equation.matchAll(/\((.*?)\)/g);
-  var matchValue = matches.next();
-  var parenthesis = [];
-  var solved;
+  var regexPAR = /\((.*?)\)/;
+  var regexEXP = /[0-9]+(\^)[0-9]+/;
+  var regexPD = /[0-9]+(\*|\/)[0-9]+/;
+  var regexAS = /[0-9]+(\+|-)[0-9]+/;
   var newValue;
-  var offset = 0;
-  var halfA;
-  var halfB;
-  console.log('working on ' + equation, matches);
-  console.log(typeof matches.value !== 'undefined' && !matches.done);
+  var test;
 
+  /**
+  * @function spliceSolvedParts
+  * @description Splices solved parts into an equation
+  * @param {string} equation Equation to splice into
+  * @param {string} solvedPart Solved part to spice into equation
+  * @param {string} unsolvedPart Unsolved part used to determine part to splice into
+  * @param {number} startIndex Starting index of the solved/unsolved part
+  * @returns {string} equation with solvedPart spliced in
+  */
+  function spliceSolvedParts(equation, solvedPart, unsolvedPart, startIndex) {
+    var halfA = equation.substr(0, startIndex);
+    var halfB = equation.substr(startIndex + unsolvedPart.length, equation.length - 1);
+    equation = halfA + solvedPart + halfB;
+    return equation;
+  }
+
+  function solveOperator(regex, equation) {
+    var solved;
+
+    test = regex.exec(equation);
+    while (test) {
+      newValue = test[0].split(test[1]).map(function(element) {
+        return parseFloat(element);
+      });
+
+      switch (test[1]) {
+        case '^':
+          solved = Math.pow(newValue[0], newValue[1]);
+          break;
+        case '*':
+          solved = newValue[0] * newValue[1];
+          break;
+        case '/':
+          solved = newValue[0] / newValue[1];
+          break;
+        case '+':
+          solved = newValue[0] + newValue[1];
+          break;
+        case '-':
+          solved = newValue[0] - newValue[1];
+          break;
+      }
+
+      equation = spliceSolvedParts(equation, solved, test[0], test.index);
+      test = regex.exec(equation);
+    }
+
+    return equation;
+  }
+
+  // No empty string allowed
   if (equation.length === 0) {
     return equation;
   }
 
-  while (typeof value !== 'undefined' && !matchValue.done) {
-    parenthesis.push({solved: solve(matchValue.value[0]), start: matches.index, originalLength: matchValue.value[0].toString().length});
-    matchValue = matches.next();
+  // Solve Parenthesis
+  test = regexPAR.exec(equation);
+  while (test) {
+    equation = spliceSolvedParts(equation, solve(test[1]), test[0], test.index);
+    test = regexPAR.exec(equation);
   }
-
-  // Replace solved parenthesis
-  while (parenthesis.length !== 0) {
-    solved = parenthesis.pop();
-    halfA = equation.substr(offset, solved.start - 1);
-    halfB = equation.substr(solved.start + solved.originalLength + 1, equation.length - 1);
-    equation = halfA + solved.answer + halfB;
-    offset = solved.start + solved.answer.toString().length;
-  }
-
-  offset = 0;
 
   // Solve exponents
-  matches = equation.matchAll(/[0-9]+\^[0-9]+/);
-  matchValue = matches.next();
-  console.log(matchValue);
-  while (typeof matchValue.value !== 'undefined' && !matchValue.done) {
-    newValue = matchValue.value[0].split('^').map(function(element) {
-      return parseFloat(element);
-    });
-    parenthesis.push({answer: Math.pow(newValue[0], newValue[1]), start: matchValue.value.index, originalLength: matchValue.value[0].toString().length});
-
-    matchValue = matches.next();
-  }
-
-  offset = 0;
-
-  // Replace solved exponents
-  while (parenthesis.length !== 0) {
-    solved = parenthesis.pop();
-    halfA = equation.substr(offset, solved.start);
-    halfB = equation.substr(solved.start + solved.originalLength, equation.length - 1);
-    equation = halfA + solved.answer + halfB;
-    offset = solved.start + solved.answer.toString().length;
-  }
+  equation = solveOperator(regexEXP, equation);
 
   // Solve multiplication / division
-  var PDRegex = /[0-9]+(\*|\/)[0-9]+/;
-  var test = PDRegex.exec(equation);
-  while (test) {
-    newValue = test[0].split(test[1]);
-    solved;
-    newValue = newValue.map(function(element) {
-      return parseFloat(element);
-    });
-
-    switch (test[1]) {
-      case '*':
-        solved = newValue[0] * newValue[1];
-        break;
-      case '/':
-        solved = newValue[0] / newValue[1];
-        break;
-    }
-
-    halfA = equation.substr(offset, test.index);
-    halfB = equation.substr(test.index + test[0].length, equation.length - 1);
-    equation = halfA + solved + halfB;
-    offset += test.index + solved.toString().length;
-
-    test = PDRegex.exec(equation);
-  }
+  equation = solveOperator(regexPD, equation);
 
   // Solve addition/subtraction
-  PDRegex = /[0-9]+(\+|\-)[0-9]+/;
-  test = PDRegex.exec(equation);
-  while (test) {
-    newValue = test[0].split(test[1]);
-    solved;
-    newValue = newValue.map(function(element) {
-      return parseFloat(element);
-    });
-
-    console.log(newValue);
-
-    switch (test[1]) {
-      case '+':
-        solved = newValue[0] + newValue[1];
-        break;
-      case '-':
-        solved = newValue[0] - newValue[1];
-        break;
-    }
-
-    halfA = equation.substr(offset, test.index);
-    halfB = equation.substr(test.index + test[0].length, equation.length - 1);
-    equation = halfA + solved + halfB;
-    offset += test.index + solved.toString().length;
-
-    test = PDRegex.exec(equation);
-  }
+  equation = solveOperator(regexAS, equation);
 
   return equation;
 }
 
-console.log(solve('4^2+(4^(1+2)+1)'), Math.pow(4, 2) + (Math.pow(4, 1 + 2) + 1));
+console.log(solve('4^2+(4^3+1)+(5*2)'), Math.pow(4, 2) + (Math.pow(4, 1 + 2) + 1));
